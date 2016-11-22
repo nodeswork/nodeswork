74b53138ab73f7789c67f4915fc7350e80e4263e
Yizhen Zhao <andy@nodeswork.com>
2016-11-21 21:19:46 -0800

    Move sample server outside.

---
+++ samples/simple_server.js
@@ -0,0 +1,65 @@
+var ExecutionCounterTask, ExecutionCounterTaskSchema, User, UserSchema, co, mongoose, nodeswork, nw;
+
+co = require('co');
+
+mongoose = require('mongoose');
+
+nodeswork = require('../lib/index');
+
+nw = new nodeswork.Nodeswork({
+  moduleName: 'simple_server'
+});
+
+UserSchema = mongoose.Schema({
+  username: String
+}, {
+  collection: 'users'
+});
+
+User = mongoose.model('User', UserSchema);
+
+nw.model(User, {
+  apiExposed: {
+    methods: ['get', 'create', 'update', 'delete'],
+    urlName: 'User',
+    path: '/users/:userId',
+    params: {
+      userId: '@_id'
+    }
+  }
+});
+
+ExecutionCounterTaskSchema = nw.Models.Task.schema.extend({
+  numOfExecutions: {
+    type: Number,
+    "default": 0
+  }
+});
+
+ExecutionCounterTaskSchema.methods.execute = function(nw) {
+  console.log('?????????????????????????????', this.numOfExecutions);
+  return this.numOfExecutions++;
+};
+
+ExecutionCounterTask = mongoose.model('ExecutionCounterTask', ExecutionCounterTaskSchema);
+
+nw.task(ExecutionCounterTask);
+
+co(function*() {
+  var ins, task;
+  ins = (yield ExecutionCounterTask.findOne({}));
+  if (ins == null) {
+    task = (yield ExecutionCounterTask.create({
+      scheduler: {
+        kind: 'SINCE_LAST_EXECUTION',
+        duration: 10000,
+        priority: 3
+      }
+    }));
+  }
+  ins.status = 'IDLE';
+  yield ins.save();
+  return nw.Models.Task.startTaskExecutor();
+});
+
+nw.start();
