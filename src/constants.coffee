f9f695f2f2ac8ed8f8cc72243b4bc70c6107aa40
Yizhen Zhao <andy@nodeswork.com>
2017-07-09 13:06:59 -0700

    Convert constants to TypeScript.

--- src/constants.coffee
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-  API_PREFIX:           '/api/v1'
-
-  PROCESSING_URL_PATH:  '/process'
-  VIEW_URL_PATH:        '/views'
-  ACTION_URL_PATH:      '/actions'
-}
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const API_PREFIX           = '/api/v1';
+export const PROCESSING_URL_PATH  = '/process';
+export const VIEW_URL_PATH        = '/views';
+export const ACTION_URL_PATH      = '/actions';
