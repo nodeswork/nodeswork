db65e7c818fcf1376b573e348b724db8bd51bf4f
Yizhen Zhao <yizhen@uber.com>
2017-09-18 14:12:40 -0700

    Add getBeans & getSingletonBeans to injection.

--- src/core/injection.ts
+++ src/core/injection.ts
@@ -7,62 +7,104 @@ export const injectionMetadataKey = Symbol('nw:injection');
 export type Beans = {[key: string]: Constructor[]};
 
 export type Constructor = {new(...args:any[]): {}};
+export interface ConstructorOverride {
+  provide:   string;
+  useClass:  Constructor;
+  multi?:    boolean;
+}
+
+function isConstructor(arg: Constructor | ConstructorOverride): arg is Constructor {
+  return typeof arg === 'function';
+}
 
 export class BeanProvider {
 
   private beans: Beans = {};
-  private singletons: {[key: string]: any} = {};
+  private singletons: {[key: string]: any[]} = {};
 
-  register(val: Constructor) {
-    const key = val.name;
+  register(val: Constructor|ConstructorOverride) {
+    let override;
+    if (isConstructor(val)) {
+      override = {
+        provide:   val.name,
+        useClass:  val,
+        multi:     false,
+      };
+    } else {
+      override = val;
+    }
+
+    const key = override.provide;
     const exists = this.beans[key];
-    if (exists != null && exists.length && exists[0] !== val) {
+
+    if (!override.multi &&
+      exists != null && exists.length && exists[0] !== val
+    ) {
       throw new Error(`Bean {${key}} already registered`);
     }
-    this.beans[key] = [val];
+    if (this.beans[key] == null) {
+      this.beans[key] = [];
+    }
+    this.beans[key] = _.union(this.beans[key], [override.useClass]);
   }
 
   getBean<T>(key: string, inputs: any[] = []): T {
+    const instances = this.getBeans<T>(key, inputs, { limit: 1 });
+    return instances[0];
+  }
+
+  getBeans<T>(
+    key: string, inputs: any[] = [], options: {limit?: number} = {},
+  ): T[] {
     const constructors = this.beans[key];
-    if (constructors == null || constructors[0] == null) {
+    if (constructors == null) {
       throw new Error(`bean {${key}} is not registed`);
     }
 
-    let injectionMetadata: InjectionMetadata = (
-      Reflect.getMetadata(injectionMetadataKey, constructors[0].prototype) || {}
-    );
-
-    const args = _.map(injectionMetadata.injects, (inject) => {
-      return this.getSingletonBean(inject.ref);
-    });
-
-    const instance = new (constructors[0].bind.apply(
-      constructors[0], [null].concat(args),
-    ));
-
-    _.each(injectionMetadata.inputs, (input) => {
-      const candidates = _.filter(
-        inputs, (i) => i.constructor.name === input.ref,
+    const firstConstructors = _.first(constructors, options.limit || 1);
+    const result = _.map(firstConstructors, (constructor) => {
+      let injectionMetadata: InjectionMetadata = (
+        Reflect.getMetadata(injectionMetadataKey, constructor.prototype) || {}
       );
-      if (input.isArray) {
-        instance[input.propertyName] = candidates;
-      } else if (candidates.length > 0) {
-        instance[input.propertyName] = candidates[0];
-      } else {
-        instance[input.propertyName] = this.getBean(input.ref, inputs);
-      }
+
+      const args = _.map(injectionMetadata.injects, (inject) => {
+        return this.getSingletonBean(inject.ref);
+      });
+
+      const instance = new (constructor.bind.apply(
+        constructor, [null].concat(args),
+      ));
+
+      _.each(injectionMetadata.inputs, (input) => {
+        const candidates = _.filter(
+          inputs, (i) => i.constructor.name === input.ref,
+        );
+        if (input.isArray) {
+          instance[input.propertyName] = candidates;
+        } else if (candidates.length > 0) {
+          instance[input.propertyName] = candidates[0];
+        } else {
+          instance[input.propertyName] = this.getBean(input.ref, inputs);
+        }
+      });
+      _.each(injectionMetadata.posts, (post) => {
+        instance[post]();
+      });
+      return instance;
     });
-    _.each(injectionMetadata.posts, (post) => {
-      instance[post]();
-    });
-    return instance;
+    return result;
   }
 
   getSingletonBean<T>(key: string): T {
+    const vals: T[] = this.getSingletonBeans(key);
+    return vals[0];
+  }
+
+  getSingletonBeans<T>(key: string): T[] {
     if (!(key in this.singletons)) {
-      this.singletons[key] = this.getBean(key);
+      this.singletons[key] = this.getBeans(key);
     }
-    return this.singletons[key] as T;
+    return this.singletons[key] as T[];
   }
 
   getRegisteredBeans(): Constructor[] {
